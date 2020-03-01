e6dec85a05109d6d855e2595d7e26e81538ca3ac
root <root@junkie.univaq.it>
2020-03-01 01:50:29 +0100

    Various adjustments: made Maven build parallel with nproc

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # jfr-jmc
 Java Flight Recorder (JFR) and Java Mission Control (JMC) Proof of Concept
+
+1) Build Spring PetClinic Docker image with:
+docker-compose build --no-cache --force-rm --parallel
+
+2) Initialize a Docker Swarm Manager node (Single node cluster on local environment):
+docker swarm init
+
+3) Ensure the Swarm Mode is activated:
+docker info | grep -Ei swarm
+
+4) Deploy the service to the cluster:
+docker stack deploy -c docker-compose.yml petclinic
+
+You're ready to go. Open Java Mission Control 7 and connect to localhost:9090 to access the JVM metrics.
--- spring-petclinic/build.sh
+++ spring-petclinic/build.sh
@@ -10,13 +10,13 @@
 
 # We're building on plain native Linux
 if [ "$OSTYPE" == "linux-gnu" ]; then
-	DOCKER_BUILDKIT=1 docker build --no-cache --force-rm -t spring-petclinic:latest .
+	DOCKER_BUILDKIT=1 docker build --no-cache --force-rm -t spring-petclinic:1.0 .
 # No, we're building on Windows, native CMD or Powershell terminal
 elif [ "$OSTYPE" == "win32" ]; then
-        docker build --no-cache --force-rm -t spring-petclinic:latest .
+        docker build --no-cache --force-rm -t spring-petclinic:1.0 .
 # Wait, we're on Windows but we're building using msys termina (eg. git bash)
 elif [ "$OSTYPE" == "msys" ]; then 
-	winpty docker build --no-cache --force-rm -t spring-petclinic:latest .
+	winpty docker build --no-cache --force-rm -t spring-petclinic:1.0 .
 else
 # Cannot build: we don't know where we are.
 	echo "Cannot determine operating system. OSTYPE var is set to ${OSTYPE}. Exiting..."
